78945eb7ccde16a7bab45517b808db22a4ee7a5e
Anthony Webster <ajwebster1@gmail.com>
2022-11-18 14:20:58 -0500

    Add function to check if all labels were defined.

--- Gerald.ConsoleApp/Assembler.fs
+++ Gerald.ConsoleApp/Assembler.fs
@@ -1,2 +1,17 @@
 module Gerald.ConsoleApp.Assembler
 
+open Gerald.ConsoleApp.Utils
+open Gerald.ConsoleApp.Parser
+open Parser
+
+let private extractJmpLabel (n: AsmLine) =
+    match n with
+    | InstructionLine (Jmp b) -> Some b
+    | _ -> None
+
+let allLabelsDefined (labels: TLabel Set) (jumpInstrs: ProgramSection): bool =
+    jumpInstrs
+    |> flatMap (extractJmpLabel >> catOption)
+    |> List.forall (fun x -> Set.contains x labels)
+    
+
